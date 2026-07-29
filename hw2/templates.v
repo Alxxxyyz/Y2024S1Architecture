ae7c8b9817c265b451cae04bca4c0d235cfbb100
Alex <471247@niuitmo.ru>
2026-07-29 17:13:57 +0300

    feat(hw2): add xor, or, and, not functions from hw1

--- hw2/templates.v
+++ hw2/templates.v
@@ -44,3 +44,120 @@ module counter(clk, addr, control, immediate, data);
   output [3:0] data; // Данные из значения под номером addr, подающиеся на выход
   // TODO: implementation
 endmodule
+
+module xor_gate(
+    input  wire in1,
+    input  wire in2,
+    output wire out
+);
+  wire and_gate_1_out;
+  wire and_gate_2_out;
+  wire not_gate_1_out;
+  wire not_gate_2_out;
+
+  not_gate not_gate_1(
+      .in(in1),
+      .out(not_gate_1_out)
+  );
+
+  not_gate not_gate_2(
+      .in(in2),
+      .out(not_gate_2_out)
+  );
+
+  and_gate and_gate_1(
+      .in1(in1),
+      .in2(not_gate_2_out),
+      .out(and_gate_1_out
+  )
+  );
+
+  and_gate and_gate_2(
+      .in1(not_gate_1_out),
+      .in2(in2),
+      .out(and_gate_2_out)
+  );
+
+  or_gate or_gate_1(
+      .in1(and_gate_1_out),
+      .in2(and_gate_2_out),
+      .out(out)
+  );
+endmodule
+
+module or_gate(
+    input  wire in1,
+    input  wire in2,
+    output wire out
+);
+  wire nor_gate_1_out;
+
+  nor_gate nor_gate_1(
+      .in1(in1),
+      .in2(in2),
+      .out(nor_gate_1_out)
+  );
+  not_gate not_gate_1(
+      .in(nor_gate_1_out),
+      .out(out)
+  );
+endmodule
+
+module and_gate(
+    input  wire in1,
+    input  wire in2,
+    output wire out
+);
+  wire nand_gate_1_out;
+
+  nand_gate nand_gate_1(
+      .in1(in1),
+      .in2(in2),
+      .out(nand_gate_1_out)
+  );
+  not_gate not_gate_1(
+      .in(nand_gate_1_out),
+      .out(out)
+  );
+endmodule
+
+module nand_gate(
+    input  wire in1,
+    input  wire in2,
+    output wire out
+);
+  supply1 pwr;
+  supply0 gnd;
+  wire n1_out;
+
+  pmos p1(out, pwr, in1);
+  pmos p2(out, pwr, in2);
+  nmos n1(out, n1_out, in1);
+  nmos n2(n1_out, gnd, in2);
+endmodule
+
+module nor_gate(
+    input  wire in1,
+    input  wire in2,
+    output wire out
+);
+  supply0 gnd;
+  supply1 pwr;
+  wire w;
+
+  pmos p1(w, pwr, in1);
+  pmos p2(out, w, in2);
+  nmos n1(out, gnd, in1);
+  nmos n2(out, gnd, in2);
+endmodule
+
+module not_gate(
+    input  wire in,
+    output wire out
+);
+  supply1 pwr;
+  supply0 gnd;
+
+  pmos p1 (out, pwr, in);
+  nmos n1 (out, gnd, in);
+endmodule
