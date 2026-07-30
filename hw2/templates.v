3d7876ac8d72c1108c6dc9407521bc44e5f53fc0
Alex <471247@niuitmo.ru>
2026-07-30 14:53:11 +0300

    feat(hw2): add mux

--- hw2/templates.v
+++ hw2/templates.v
@@ -45,15 +45,96 @@ module counter(clk, addr, control, immediate, data);
   // TODO: implementation
 endmodule
 
-module ripple_adder(
-    input wire []
+module mux4(
+    input  wire [1:0] control,
+    input  wire       data_1,
+    input  wire       data_2,
+    input  wire       data_3,
+    input  wire       data_4,
+    output wire       out
 );
+  wire first_half;
+  wire last_half;
+  mux first_half_mux(
+      .control(control[0]),
+      .data_1(data_1),
+      .data_2(data_2),
+      .out(first_half)
+  );
+  mux last_half_mux(
+      .control(control[0]),
+      .data_1(data_3),
+      .data_2(data_4),
+      .out(last_half)
+  );
+  mux total_mux(
+      .control(control[1]),
+      .data_1(first_half),
+      .data_2(last_half),
+      .out(out)
+  );
+endmodule
 
+module mux(
+    input  wire control,
+    input  wire data_1,
+    input  wire data_2,
+    output wire out
+);
+  wire control_invert;
+  wire data_1_pass;
+  wire data_2_pass;
+
+  not control_invert_gate(control_invert, control);
+  or  data_1_pass_gate(data_1_pass, control_invert, data_1);
+  or  data_2_pass_gate(data_2_pass, control, data_2);
+  and data_output_gate(out, data_1_pass, data_2_pass);
+endmodule
+
+module ripple_adder(
+    input  wire [3:0] addend_1,
+    input  wire [3:0] addend_2,
+    input  wire       carry_in,
+    output wire       carry_out,
+    output wire [3:0] sum
+);
+  wire carry_1;
+  wire carry_2;
+  wire carry_3;
+
+  full_adder least_significant_bit(
+      .addend_1(addend_1[0]),
+      .addend_2(addend_2[0]),
+      .carry_in(carry_in),
+      .carry_out(carry_1),
+      .sum(sum[0])
+  );
+  full_adder second_bit(
+      .addend_1(addend_1[1]),
+      .addend_2(addend_2[1]),
+      .carry_in(carry_1),
+      .carry_out(carry_2),
+      .sum(sum[1])
+  );
+  full_adder third_bit(
+      .addend_1(addend_1[2]),
+      .addend_2(addend_2[2]),
+      .carry_in(carry_2),
+      .carry_out(carry_3),
+      .sum(sum[2])
+  );
+  full_adder most_significant_bit(
+      .addend_1(addend_1[3]),
+      .addend_2(addend_2[3]),
+      .carry_in(carry_3),
+      .carry_out(carry_out),
+      .sum(sum[3])
+  );
 endmodule
 
 module full_adder(
-    input  wire addend1,  // A
-    input  wire addend2,  // B
+    input  wire addend_1,  // A
+    input  wire addend_2,  // B
     input  wire carry_in,
     output wire carry_out,
     output wire sum
@@ -61,185 +142,20 @@ module full_adder(
   wire AB;
   wire ACin;
   wire BCin;
-  wire ACin_or_BCin;
-  wire A_XOR_B;
 
-  and_gate AB_gate(
-      .in1(addend1),
-      .in2(addend2),
-      .out(AB)
-  );
-
-  and_gate ACin_gate(
-      .in1(addend1),
-      .in2(carry_in),
-      .out(ACin)
-  );
-
-  and_gate BCin_gate(
-      .in1(addend2),
-      .in2(carry_in),
-      .out(BCin)
-  );
-
-  or_gate ACin_or_BCin_gate(
-      .in1(ACin),
-      .in2(BCin),
-      .out(ACin_or_BCin)
-  );
-
-  or_gate carry_out_gate(
-      .in1(AB),
-      .in2(ACin_or_BCin),
-      .out(carry_out)
-  );
-
-  xor_gate A_XOR_B_gate(
-    .in1(addend1),
-    .in2(addend2),
-    .out(A_XOR_B)
-  );
-
-  xor_gate sum_gate(
-      .in1(A_XOR_B),
-      .in2(carry_in),
-      .out(sum)
-  );
+  xor sum_gate(sum, addend_1, addend_2, carry_in);
+  and and_1(AB, addend_1, addend_2);
+  and and_2(ACin, addend_1, carry_in);
+  and and_3(BCin, addend_2, carry_in);
+  or  carry_out_gate(carry_out, AB, ACin, BCin);
 endmodule
 
 module half_adder(
-    input  wire addend1,
-    input  wire addend2,
+    input  wire addend_1,
+    input  wire addend_2,
     output wire carry_out,
     output wire sum
 );
-
-  and_gate carry_out_gate(
-      .in1(addend1),
-      .in2(addend2),
-      .out(carry_out)
-  );
-
-  xor_gate sum_gate(
-      .in1(addend1),
-      .in2(addend2),
-      .out(sum)
-  );
-endmodule
-
-module xor_gate(
-    input  wire in1,
-    input  wire in2,
-    output wire out
-);
-  wire and_gate_1_out;
-  wire and_gate_2_out;
-  wire not_gate_1_out;
-  wire not_gate_2_out;
-
-  not_gate not_gate_1(
-      .in(in1),
-      .out(not_gate_1_out)
-  );
-
-  not_gate not_gate_2(
-      .in(in2),
-      .out(not_gate_2_out)
-  );
-
-  and_gate and_gate_1(
-      .in1(in1),
-      .in2(not_gate_2_out),
-      .out(and_gate_1_out
-  )
-  );
-
-  and_gate and_gate_2(
-      .in1(not_gate_1_out),
-      .in2(in2),
-      .out(and_gate_2_out)
-  );
-
-  or_gate or_gate_1(
-      .in1(and_gate_1_out),
-      .in2(and_gate_2_out),
-      .out(out)
-  );
-endmodule
-
-module or_gate(
-    input  wire in1,
-    input  wire in2,
-    output wire out
-);
-  wire nor_gate_1_out;
-
-  nor_gate nor_gate_1(
-      .in1(in1),
-      .in2(in2),
-      .out(nor_gate_1_out)
-  );
-  not_gate not_gate_1(
-      .in(nor_gate_1_out),
-      .out(out)
-  );
-endmodule
-
-module and_gate(
-    input  wire in1,
-    input  wire in2,
-    output wire out
-);
-  wire nand_gate_1_out;
-
-  nand_gate nand_gate_1(
-      .in1(in1),
-      .in2(in2),
-      .out(nand_gate_1_out)
-  );
-  not_gate not_gate_1(
-      .in(nand_gate_1_out),
-      .out(out)
-  );
-endmodule
-
-module nand_gate(
-    input  wire in1,
-    input  wire in2,
-    output wire out
-);
-  supply1 pwr;
-  supply0 gnd;
-  wire n1_out;
-
-  pmos p1(out, pwr, in1);
-  pmos p2(out, pwr, in2);
-  nmos n1(out, n1_out, in1);
-  nmos n2(n1_out, gnd, in2);
-endmodule
-
-module nor_gate(
-    input  wire in1,
-    input  wire in2,
-    output wire out
-);
-  supply0 gnd;
-  supply1 pwr;
-  wire w;
-
-  pmos p1(w, pwr, in1);
-  pmos p2(out, w, in2);
-  nmos n1(out, gnd, in1);
-  nmos n2(out, gnd, in2);
-endmodule
-
-module not_gate(
-    input  wire in,
-    output wire out
-);
-  supply1 pwr;
-  supply0 gnd;
-
-  pmos p1(out, pwr, in);
-  nmos n1(out, gnd, in);
+  and carry_out_gate(carry_out, addend_1, addend_2);
+  xor sum_gate(sum, addend_1, addend_2);
 endmodule
